297aa9c9da241f1659b90c3009640e41674460b0
HTechMedia <85754896+HTechMediaYT@users.noreply.github.com>
2021-11-03 21:52:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -111,6 +111,9 @@ async def next_page(bot, query):
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
+           
+            for file in files
+        ]
      else:
          Send_message = await message.reply_text(
                  text=f"<b>Hello {message.from_user.first_name}, I could not find {search} the movie you asked for...\n\nGoogle,IMDB-Click on any button and find the CORRECT MOVIE NAME and enter it here but the movie will be available...If you do not receive the movie even after entering the correct name ...  @admin type movie name Inform the admin in this format...We will upload within 24 hours</b>",
@@ -123,11 +126,7 @@ async def next_page(bot, query):
                       ]
                   )
              )
-           
-            for file in files
-        ]
-         return
-      
+      return
 
     if 0 < offset <= 10:
         off_set = 0
